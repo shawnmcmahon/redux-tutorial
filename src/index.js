2284b3c17436c3ff09ffb06b5322607830b0ef20
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-12-09 21:02:12 -0700

    Add functional programming example 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import { compose, pipe } from  'loadash/fp'; 
+
+let input = "    Javascript    "; 
+let output = "<div>" + input.trim() + "</div>";
+
+const trim = str = str.trim(); 
+const wrapInDiv = str => `<div>${str}</div>`;
+const toLowerCase = str => str.toLowerCase();
+
+const result = wrapInDiv(toLowerCase(trim(input))); 
+
+
