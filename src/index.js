47f02e5cb96e46b3e2c52833fa52ef52fdc347f3
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-12-09 22:05:36 -0700

    ???

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import { compose, pipe } from  'loadash/fp'; 
+import { compose, pipe } from  'lodash/fp'; 
 
 let input = "    Javascript    "; 
 let output = "<div>" + input.trim() + "</div>";
 
 const trim = str => str.trim(); 
-const wrapInDiv = str => `<div>${str}</div>`;
+const wrap = (type, str) => `<${type}>${str}</${type}>`;
 const toLowerCase = str => str.toLowerCase();
 
-const transform  = pipe(trim, toLowerCase, wrapInDiv); 
+const transform  = pipe(trim, toLowerCase, wrap); 
 
 transform(input)
+console.log(transform(input))
 
 
 
