a40708c3bfddcdc31995b03c21df006136427f12
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-12-09 21:04:42 -0700

    import compose and pipe logic

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import { compose, pipe } from  'loadash/fp';
 let input = "    Javascript    "; 
 let output = "<div>" + input.trim() + "</div>";
 
-const trim = str = str.trim(); 
+const trim = str => str.trim(); 
 const wrapInDiv = str => `<div>${str}</div>`;
 const toLowerCase = str => str.toLowerCase();
 
-const result = wrapInDiv(toLowerCase(trim(input))); 
+const transform  = pipe(trim, toLowerCase, wrapInDiv); 
+
+transform(input)
+
 
 
